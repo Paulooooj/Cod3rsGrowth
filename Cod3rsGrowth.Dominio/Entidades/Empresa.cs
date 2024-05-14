fd1b51955556c643903764ba305a5708f535894e
Paulo Sergio <paulosergio_gyn@hotmail.com>
2024-05-14 13:52:18 -0300

    criado as classes do projeto

---
+++ Cod3rsGrowth.Dominio/Entidades/Empresa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cod3rsGrowth.Dominio.Entidades
+{
+    public class Empresa
+    {
+        public int Id { get; set; }
+        public string RazaoSocial { get; set; }
+        public string CNPJ { get; set; }
+        public EnumRamoDaEmpresa Ramo { get; set; }
+
+    }
+}
---
+++ Cod3rsGrowth.Dominio/Entidades/Produtos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cod3rsGrowth.Dominio.Entidades
+{
+    public class Produtos
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal ValorDoProduto { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public bool TemDataValida { get; set; }
+        public DateTime DataValidade { get; set; }
+        public int EmpresaId { get; set; }
+    }
+
+}
