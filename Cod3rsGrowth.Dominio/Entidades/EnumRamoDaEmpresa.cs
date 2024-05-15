5a4e0cc52c94d742c003e29b9b8e5768269118b9
Paulo Sergio <paulosergio_gyn@hotmail.com>
2024-05-15 08:35:18 -0300

    adicionado descricao para os enum

--- Cod3rsGrowth.Dominio/Entidades/EnumRamoDaEmpresa.cs
+++ Cod3rsGrowth.Dominio/Entidades/EnumRamoDaEmpresa.cs
@@ -1,15 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System.ComponentModel;
+
 
 namespace Cod3rsGrowth.Dominio.Entidades
 {
     public enum EnumRamoDaEmpresa
     {
+        [Description("Tem como objetivo produção de produtos")]
         Industria,
+        [Description("Movimenta diferentes tipos de produtos")]
         Comercio,
+        [Description("Foco na venda do seu serviço")]
         Servico
     }
 }
