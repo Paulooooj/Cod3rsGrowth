3e0be2380ab8e9215f86485702a2d3e8f60e1e35
Paulo Sergio <paulosergio_gyn@hotmail.com>
2024-06-06 12:17:23 -0300

    criado os repositorio na interface e conexão ao banco de dados

---
+++ Cod3rsGrowth.Infra/Repositorio/RepositorioEmpresa.cs
@@ -0,0 +1,39 @@
+﻿using Cod3rsGrowth.Dominio.Entidades;
+using Cod3rsGrowth.Infra.Interfaces;
+using LinqToDB.Data;
+using System.Collections.Generic;
+
+namespace Cod3rsGrowth.Infra.Repositorio
+{
+    public class RepositorioEmpresa : IRepositorioEmpresa
+    {
+        DataConnection db;
+        public RepositorioEmpresa()
+        {
+        }
+        public void Adicionar(Empresa empresa)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Atualizar(Empresa empresa)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Deletar(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Empresa ObterPorId(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Empresa> ObterTodos()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Cod3rsGrowth.Infra/Repositorio/RepositorioProduto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cod3rsGrowth.Infra.Repositorio
+{
+    internal class RepositorioProduto
+    {
+    }
+}
