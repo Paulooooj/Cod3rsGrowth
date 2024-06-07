14f5705cf277d337f2c41b87857a9a28a1973f4e
Paulo Sergio <paulosergio_gyn@hotmail.com>
2024-06-07 08:41:33 -0300

    Atualizando a classe repositorioProduto e corrigindo o código

--- Cod3rsGrowth.Infra/Repositorio/RepositorioEmpresa.cs
+++ Cod3rsGrowth.Infra/Repositorio/RepositorioEmpresa.cs
@@ -1,16 +1,15 @@
 ﻿using Cod3rsGrowth.Dominio.Entidades;
 using Cod3rsGrowth.Infra.Interfaces;
-using LinqToDB.Data;
 using System.Collections.Generic;
 
 namespace Cod3rsGrowth.Infra.Repositorio
 {
     public class RepositorioEmpresa : IRepositorioEmpresa
     {
-        DataConnection db;
         public RepositorioEmpresa()
         {
         }
+
         public void Adicionar(Empresa empresa)
         {
             throw new System.NotImplementedException();
--- Cod3rsGrowth.Infra/Repositorio/RepositorioProduto.cs
+++ Cod3rsGrowth.Infra/Repositorio/RepositorioProduto.cs
@@ -1,10 +1,35 @@
-﻿using System;
+﻿using Cod3rsGrowth.Dominio.Entidades;
+using Cod3rsGrowth.Infra.Interfaces;
+using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Cod3rsGrowth.Infra.Repositorio
 {
-    internal class RepositorioProduto
+    public class RepositorioProduto : IRepositorioProduto
     {
+        public void Adicionar(Produto produto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Atualizar(Produto produto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Deletar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Produto ObterPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Produto> ObterTodos()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
