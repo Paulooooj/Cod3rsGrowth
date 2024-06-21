2e8f69a0b3f3d2e9a6ad598d839309b2501c57ef
Paulo Sergio <paulosergio_gyn@hotmail.com>
2024-06-21 10:24:43 -0300

    Atualizando o nome do Forms

--- Cod3rsGrowth.Forms/Program.cs
+++ Cod3rsGrowth.Forms/Program.cs
@@ -4,18 +4,13 @@ namespace Cod3rsGrowth.Forms
 {
     public static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
             var contextoMigracoes = new Migracoes();
             contextoMigracoes.ExecutarMigracao();
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new FormListaEmpresa());
         }
     }
 }
