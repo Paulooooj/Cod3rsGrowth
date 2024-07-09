ac13f6475a6afe1d6b6b20f4bcb3d65a3dc571b1
Paulo Sergio <paulosergio_gyn@hotmail.com>
2024-07-09 12:02:24 -0300

    Criado a injeção de dependência e as controller e seus endpoint

---
+++ Cod3rsGrowth.Web/Controllers/ProdutoController.cs
@@ -0,0 +1,32 @@
+﻿using Cod3rsGrowth.Dominio.Entidades;
+using Cod3rsGrowth.Servico.Servicos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cod3rsGrowth.Web.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private readonly ServicoProduto _servicoProduto;
+
+        public ProdutoController(ServicoProduto servicoProduto)
+        {
+            _servicoProduto = servicoProduto;
+        }
+
+        [HttpGet]
+        public IActionResult ObterTodos() { throw new NotImplementedException(); }
+
+        [HttpGet("{id}")]
+        public IActionResult ObterPorId(int id) { throw new NotImplementedException(); }
+
+        [HttpPost]
+        public IActionResult Adicionar(Empresa empresa) { throw new NotImplementedException(); }
+
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id) { throw new NotImplementedException(); }
+
+        [HttpPut]
+        public IActionResult Atualizar(Empresa empresa) { throw new NotImplementedException(); }
+
+    }
+}
--- Cod3rsGrowth.Web/Program.cs
+++ Cod3rsGrowth.Web/Program.cs
@@ -1,11 +1,40 @@
+using Cod3rsGrowth.Dominio.Entidades;
+using Cod3rsGrowth.Dominio.Migrations;
+using Cod3rsGrowth.Dominio.Servicos;
+using Cod3rsGrowth.Infra;
+using Cod3rsGrowth.Infra.Interfaces;
+using Cod3rsGrowth.Infra.Repositorio;
+using Cod3rsGrowth.Servico.Servicos;
+using Cod3rsGrowth.Servico.Validacao;
+using FluentMigrator.Runner;
+using FluentValidation;
+using LinqToDB;
+using LinqToDB.AspNet;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+const string variavelAmbienteStringConexao = "ConnectionString";
+var stringConexao = Environment.GetEnvironmentVariable(variavelAmbienteStringConexao)
+    ?? throw new Exception($"Variavel de ambiente [{variavelAmbienteStringConexao}] nao encontrada");
+
+builder.Services.AddFluentMigratorCore().ConfigureRunner(rb => rb.
+AddSqlServer()
+.WithGlobalConnectionString(stringConexao)
+.ScanIn(typeof(_20240703075800_migracao_da_tabela_empresa).Assembly).For.Migrations())
+.AddLogging(lb => lb.AddFluentMigratorConsole());
+
+builder.Services.AddLinqToDBContext<DbCod3rsGrowth>((provider, options) => options.UseSqlServer(stringConexao));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ServicoEmpresa>();
+builder.Services.AddScoped<ServicoProduto>();
+builder.Services.AddScoped<IValidator<Empresa>, ValidadorEmpresa>();
+builder.Services.AddScoped<IValidator<Produto>, ValidadorProduto>();
+builder.Services.AddScoped<IRepositorioEmpresa, RepositorioEmpresa>();
+builder.Services.AddScoped<IRepositorioProduto, RepositorioProduto>();
 
 var app = builder.Build();
 
@@ -14,6 +43,11 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+using (var scope = app.Services.CreateScope())
+{
+    var runner = scope.ServiceProvider.GetRequiredService<IMigrationRunner>();
+    runner.MigrateUp();
 }
 
 app.UseHttpsRedirection();
