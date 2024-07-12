925876c2bfcc31336ec9228fbc89eaf06d353f8b
Paulo Sergio <paulosergio_gyn@hotmail.com>
2024-07-12 10:14:19 -0300

    Trocando Ok para NoContent no endpoint atualizar

--- Cod3rsGrowth.Web/Controllers/EmpresaController.cs
+++ Cod3rsGrowth.Web/Controllers/EmpresaController.cs
@@ -43,7 +43,7 @@ namespace Cod3rsGrowth.Web.Controllers
         public IActionResult Atualizar([FromBody] Empresa empresa) 
         {
             _servicoEmpresa.Atualizar(empresa);
-            return Ok();
+            return NoContent();
         }
 
     }
